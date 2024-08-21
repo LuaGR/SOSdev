e05bcba91cda06c00ec2f522c23e09b4e7fff88d
LuaAguirre <renatoag98@gmail.com>
2024-08-21 17:38:03 -0500

    check ts

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,11 +1,10 @@
-import { Key } from 'react'
 import { Item } from '@/types/item'
 import Resource from '@/components/items/resources'
 
 export default function Items({ items }: { items: Item[] }) {
   return (
     <div className='gap-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4'>
-      {items.map((item: Item, index: Key) => (
+      {items.map((item, index) => (
         <Resource
           item={item}
           index={index}
