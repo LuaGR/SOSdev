bf19259c91a438dd1b3f49eac75bb6791a63db07
LuaAguirre <renatoag98@gmail.com>
2024-09-03 05:15:39 -0500

    split items

--- app/items/items.tsx
+++ app/items/items.tsx
@@ -1,15 +1,70 @@
 import type { Item } from '@/types/item'
 import Resource from '@/components/items/resources'
+import PaginationClient from '@/components/items/pagination-client'
 
-export default function Items({ items }: { items: Item[] }) {
+import { sql } from '@vercel/postgres'
+
+export default async function Items({
+  searchParams
+}: {
+  searchParams?: {
+    query?: string
+    page?: string
+    category?: string
+  }
+}) {
+  // Fetch resources from the database
+  const { rows } = await sql`SELECT * from resources`
+
+  console.log('Fetched resources:', rows)
+
+  // Map the rows to the Item type
+  const resources: Item[] = rows.map((row) => ({
+    id: row.id,
+    title: row.title,
+    description: row.description,
+    image: row.image,
+    category: row.category,
+    url: row.url
+  }))
+
+  // Pagination
+  const itemsPerPage = 8
+  const query = searchParams?.query || ''
+  const category = searchParams?.category || 'all'
+  const page = Number(searchParams?.page || '1')
+
+  // Filters
+  const filteredResources = resources.filter((item: Item) => {
+    const matchesCategory = category === 'all' || item.category === category
+    const matchesQuery =
+      item.title.toLowerCase().includes(query.toLowerCase()) ||
+      item.description?.toLowerCase().includes(query.toLowerCase())
+    return matchesCategory && matchesQuery
+  })
+
+  const totalItems = filteredResources.length
+  const paginatedResources = filteredResources.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  )
+
   return (
-    <div className='gap-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
-      {items.map((item) => (
-        <Resource
-          item={item}
-          key={item.id}
-        />
-      ))}
-    </div>
+    <>
+      <div className='gap-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
+        {paginatedResources.map((item) => (
+          <Resource
+            item={item}
+            key={item.id}
+          />
+        ))}
+      </div>
+
+      <PaginationClient
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        currentPage={page}
+      />
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import Header from '@/components/header'
 import Filters from '../components/filters'
 import Items from './items/items'
-import PaginationClient from '@/components/items/pagination-client'
 import { Suspense } from 'react'
-import { sql } from '@vercel/postgres'
-import type { Item } from '@/types/item'
 
 export default async function Home({
   searchParams
@@ -15,39 +12,9 @@ export default async function Home({
     category?: string
   }
 }) {
-  // Fetch resources from the database
-  const { rows } = await sql`SELECT * from resources`
-
-  // Map the rows to the Item type
-  const resources: Item[] = rows.map((row) => ({
-    id: row.id,
-    title: row.title,
-    description: row.description,
-    image: row.image,
-    category: row.category,
-    url: row.url
-  }))
-
-  // Pagination
-  const itemsPerPage = 8
   const query = searchParams?.query || ''
-  const category = searchParams?.category || 'all'
+
   const page = Number(searchParams?.page || '1')
-
-  // Filters
-  const filteredResources = resources.filter((item: Item) => {
-    const matchesCategory = category === 'all' || item.category === category
-    const matchesQuery =
-      item.title.toLowerCase().includes(query.toLowerCase()) ||
-      item.description?.toLowerCase().includes(query.toLowerCase())
-    return matchesCategory && matchesQuery
-  })
-
-  const totalItems = filteredResources.length
-  const paginatedResources = filteredResources.slice(
-    (page - 1) * itemsPerPage,
-    page * itemsPerPage
-  )
 
   return (
     <div className='flex flex-col gap-12 items-center'>
@@ -56,20 +23,7 @@ export default async function Home({
       <Suspense
         key={query + page}
         fallback={<div>loading...</div>}>
-        {totalItems ? (
-          <Items items={paginatedResources} />
-        ) : (
-          <p className='text-md text-default-600 dark:text-default-400'>
-            The search found no matches.
-          </p>
-        )}
-      </Suspense>
-      <Suspense>
-        <PaginationClient
-          totalItems={totalItems}
-          itemsPerPage={itemsPerPage}
-          currentPage={page}
-        />
+        <Items searchParams={searchParams} />
       </Suspense>
     </div>
   )
