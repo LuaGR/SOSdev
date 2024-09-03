6e13543c9804ec3e5685249a664c5fbb8aa7c49f
LuaAguirre <renatoag98@gmail.com>
2024-09-02 22:15:13 -0500

    check grid

--- app/items/items.tsx
+++ app/items/items.tsx
@@ -3,7 +3,7 @@ import Resource from '@/components/items/resources'
 
 export default function Items({ items }: { items: Item[] }) {
   return (
-    <div className='gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className='gap-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
       {items.map((item) => (
         <Resource
           item={item}
