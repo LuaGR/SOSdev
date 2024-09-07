7561d6194b6b7966882a9bc2c32c7bab94f88efd
LuaAguirre <renatoag98@gmail.com>
2024-09-06 20:10:43 -0500

    check skeleton

--- components/items/skeleton-items.tsx
+++ components/items/skeleton-items.tsx
@@ -3,14 +3,14 @@ import { Skeleton } from '@nextui-org/skeleton'
 
 export default function SkeletonItems() {
   return (
-    <div className='gap-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4'>
+    <div className='gap-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 m-0 p-0'>
       {Array.from({ length: 8 }).map((_, index) => (
         <Card
           key={index}
           className='w-[300px] space-y-5 p-4'
           radius='lg'>
           <Skeleton className='rounded-lg'>
-            <div className='h-[180px] w-full rounded-lg bg-default-300'></div>
+            <div className='h-[120px] w-full rounded-lg bg-default-300'></div>
           </Skeleton>
           <div className='space-y-3'>
             <Skeleton className='w-3/5 rounded-lg'>
