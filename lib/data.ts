354edf54ff2dd20726a67cc3789ab10feff22d4f
LuaAguirre <renatoag98@gmail.com>
2024-09-09 14:43:32 -0500

    revalidate

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres'
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 0
 export async function fetchResources() {
   try {
     const data = await sql`SELECT * FROM resources`
